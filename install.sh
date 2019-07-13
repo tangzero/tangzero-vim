6783bc08755301124713c1f39571db9452c7ab25
Jairo Luiz <t4ngz3r0@gmail.com>
2019-07-13 11:58:35 -0400

    installation script and other small fixes

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # clone and install
-git clone https://github.com/tangzero/vim ~/.vim
+git clone https://github.com/tangzero/tangzero-vim ~/.vim
 
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install
+ln -ns ~/.vim ~/.config/nvim
+touch ~/.vimrc.local
+touch ~/.vimrc.local.options
+touch ~/.vimrc.local.plugins
+
+# start vim
+vim +set nomore +PlugInstall +PlugClean! +qall
 
